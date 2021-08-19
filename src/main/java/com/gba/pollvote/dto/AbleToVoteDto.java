7a64570e541d51241820a38ea5d240746e86f225
Gabriel Brandão <gbagabriel5@outlook.com>
2021-08-19 16:30:55 -0400

    fix able to vote dto implements serializable

--- src/main/java/com/gba/pollvote/dto/AbleToVoteDto.java
+++ src/main/java/com/gba/pollvote/dto/AbleToVoteDto.java
@@ -1,14 +1,15 @@
 package com.gba.pollvote.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
+import java.io.Serializable;
 
 @Getter
 @Setter
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public class AbleToVoteDto {
+public class AbleToVoteDto implements Serializable {
     private String status;
 }
